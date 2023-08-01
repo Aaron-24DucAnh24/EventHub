79b46455d78c0721f5345dee41ab271fba175a67
Aaron-24DucAnh24 <buingocducanh24@gmail.com>
2023-08-01 17:26:04 +0700

    update base url

--- ticket-booking-webapp/src/config.ts
+++ ticket-booking-webapp/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://172.190.186.255/api";
+export const BASE_URL = "https://b8c5-172-190-186-255.ngrok.io/api";
 export const PROVINCE_API = "https://provinces.open-api.vn/api/";
