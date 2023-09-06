74363d7449053a5c10bef392364330bd67ef153f
Đức Anh <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2023-09-06 13:21:40 +0700

    Update config.ts

--- ticket-booking-webapp/src/config.ts
+++ ticket-booking-webapp/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://b8c5-172-190-186-255.ngrok.io/api";
+export const BASE_URL = "http://127.0.0.1:3001/api";
 export const PROVINCE_API = "https://provinces.open-api.vn/api/";
